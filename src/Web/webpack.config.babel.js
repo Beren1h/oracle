27398263276851dbc4ed4d19bff1e8b2f3ef128a
monkeyfeathers99 <monkeyfeathers99@gmail.com>
2017-07-08 14:12:25 -0400

    working web with javascript

---
+++ src/Web/webpack.config.babel.js
@@ -0,0 +1,8 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './wwwroot/Scripts/src/entry.js',
+    output: {
+        path: path.join(__dirname, '/wwwroot/Scripts'),
+        filename: 'bundle.js'
+};
