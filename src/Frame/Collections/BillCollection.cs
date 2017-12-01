4e2b6db451b85c5a4bf550d0d0c85bef24b5884b
monkeyfeathers99 <monkeyfeathers99@gmail.com>
2017-11-30 19:16:18 -0500

    bills concept

---
+++ src/Frame/Collections/BillCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using LiteDB;
+using Frame.Models;
+using System.Linq.Expressions;
+
+namespace Frame.Collections
+{
+    public class BillCollection : IDbCollection<Bill>
+    {
+        private LiteCollection<Bill> _collection;
+
+        public BillCollection(LiteDatabase db)
+        {
+            _collection = db.GetCollection<Bill>("Bill");
+        }
+
+        public bool Delete(ObjectId _id)
+        {
+            return _collection.Delete(_id);
+        }
+
+        public IEnumerable<Bill> Get(Expression<Func<Bill, bool>> query)
+        {
+            return _collection.Find(query);
+        }
+
+        public ObjectId Insert(Bill document)
+        {
+            return _collection.Insert(document);
+        }
+
+        public bool Update(Bill document)
+        {
+            return _collection.Update(document);
+        }
+    }
+}
---
+++ src/Frame/Models/Bill.cs
@@ -0,0 +1,14 @@
+﻿using LiteDB;
+using System;
+
+namespace Frame.Models
+{
+    public class Bill
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public DateTime Next { get; set; }
+        public decimal Amount { get; set; }
+        public string Frequency { get; set; }
+    }
+}
